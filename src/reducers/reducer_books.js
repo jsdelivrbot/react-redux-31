086d55f24a4bb309ca560a28a736cb9868bce5e2
Binod Mainali <binodmainali1@gmail.com>
2018-05-02 08:55:22 +0530

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Binod Mainali: The Good Part'},
+    {title: 'C++ how to code'},
+    {title: 'Shell what to ignore'},
+    {title: 'Javascript: What to read'},
+    {title: 'Nothing Here to see'}
+  ]
+};
